96f481c2f2a9138197980b1afd930feb218d33eb
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-17 16:08:56 -0400

    js update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-const boxes = document.querySelectorAll('.box');
-const status = document.getElementById('status');
-const playNowButton = document.getElementById('play-now'); 
-const restartButton = document.getElementById('restart');
+const playNowButton = document.getElementById("play-now");
+const restartButton = document.getElementById("restart");
+const boxes = document.querySelectorAll(".box");
+
+let playNowCount = 0;
+let restartCount = 0;
+
+playNowButton.addEventListener("click", function() {
+    playNowCount++;
+    console.log("Play Now button clicked " + playNowCount + " times.");
+});
+
+restartButton.addEventListener("click", function() {
+    restartCount++;
+    console.log("Restart Game button clicked " + restartCount + " times.");
+});
+
+boxes.forEach(function(box, index) {
+    box.addEventListener("click", function() {
+    console.log("Box " + index + " clicked.");
+// Add your Tic-Tac-Toe game logic here
+});
+});
