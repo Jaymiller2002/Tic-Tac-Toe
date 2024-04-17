b213e405bb56e48fd48adb0ca558ff26ec28a9b8
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-17 15:06:36 -0400

    looks more like wireframe

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const boxes = document.querySelectorAll('.box');
+const status = document.getElementById('status');
+const playNowButton = document.getElementById('play-now'); 
+const restartButton = document.getElementById('restart');
